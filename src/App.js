4e37c9ed9b29a17d5c05eacafdf673fee429f98d
Raza-DG <ra@digitalgraphiks.pk>
2023-11-01 15:29:09 +0500

    todoApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todolist,setTodoList] = useState([]);
+  const [inputValue,setInputValue] = useState('');
+  const [editTodoId, setEditTodoId] = useState();
+  const [editTodoValue, setEditTodoValue] = useState();
+  const [updateValue,setUpdateValue] = useState('');
+
+  const addTodo = () => {
+    const newTodo = {
+      text:inputValue,
+      id:Date.now()
+    };
+    setTodoList([...todolist,newTodo]);
+    setInputValue('');
+  }
+
+  const editTodo = (id) => {
+    const todos = todolist;
+    const updatedTodos = todos.filter((todo)=>todo.id === id);
+    setEditTodoId(id);
+    setEditTodoValue(updatedTodos[0].text);
+  }
+
+  const updateTodo = () => {
+    console.log(editTodoId,updateValue);
+    // const newTodo = [...todolist];
+    // newTodo[editTodoId] = updateValue;
+    // setTodoList(newTodo);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="myDIV" className="header">
+        <h2>My To Do List</h2>
+        <input type="text" id="myInput" className='addinput' value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Title..."/>
+        <span onClick={addTodo} className="addBtn" >Add</span>
+      </div>
+      <ul id="myUL">
+        {todolist.map((todo) => (
+          <li key={todo.id}>
+            {editTodoId === todo.id ? (
+                <div>
+                  <input type="text" value={editTodoValue} onChange={(e) => setUpdateValue(e.target.value)} placeholder="Title..."/>
+                  <span onClick={updateTodo} className="updateList" >Update</span>
+                </div>
+              ):(
+                <div>
+                  <span>{todo.text}</span> <div className='action'><span className="edit" onClick={()=>editTodo(todo.id)}>Edit</span><span className="close">×</span></div>
+                </div>
+              )
+            }
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
